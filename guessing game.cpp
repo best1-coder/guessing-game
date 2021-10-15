5ee2a1283ae1bb79b298265223d4259ea76b4dbf
best1-coder <73450366+best1-coder@users.noreply.github.com>
2021-10-15 16:49:20 +0100

    Add files via upload

---
+++ guessing game.cpp
@@ -0,0 +1,297 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+
+{
+	char ans;
+	int myarr[10]={1,2,3,4,5,6,7,8,9,10};
+	int secretNumber=2;
+	
+	
+	//when the number isnt 2///////////////////////////////////////////////////////////////
+cout<<"is the number 2: ";
+cin>> ans;
+
+
+
+//if when secret number is not 2
+if (secretNumber != 2 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=2 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+
+//if number is 2
+if (secretNumber == 2 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 2 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+	
+}
+
+
+	//when the number isnt 4///////////////////////////////////////////////////////////////////////////////////////////
+cout<<"is the number 4: ";
+cin>> ans;
+//if when secret number is not 4
+if (secretNumber != 4 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=4 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+
+//if number is 4
+if (secretNumber == 4 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 4 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+
+	//when the number isnt 6///////////////////////////////////////////////////////////////
+cout<<"is the number 6: ";
+cin>> ans;
+
+
+//if when secret number is not 6
+if (secretNumber != 6 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=6 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+//if number is 6
+if (secretNumber == 6 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 6 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+	//when the number isnt 8///////////////////////////////////////////////////////////////
+cout<<"is the number 8: ";
+cin>> ans;
+//if when secret number is not 8
+if (secretNumber != 8 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=8 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+//if number is 8
+if (secretNumber == 8 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 8 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+	//when the number isnt 1///////////////////////////////////////////////////////////////
+cout<<"is the number 1: ";
+cin>> ans;
+
+
+
+//if when secret number is not 1
+if (secretNumber != 1 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=1 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+
+//if number is 1
+if (secretNumber == 1 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 1 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+
+	//when the number isnt 3///////////////////////////////////////////////////////////////
+cout<<"is the number 3: ";
+cin>> ans;
+
+
+
+//if when secret number is not 3
+if (secretNumber != 3 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=3 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+
+
+//if number is 3
+if (secretNumber == 3 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 3 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+
+	//when the number isnt 10///////////////////////////////////////////////////////////////
+cout<<"is the number 10: ";
+cin>> ans;
+
+
+
+//if when secret number is not 10
+if (secretNumber != 10 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=10 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+//if number is 10
+if (secretNumber == 10 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 10 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+	//when the number isnt 5 ///////////////////////////////////////////////////////////////
+cout<<"is the number 5: ";
+cin>> ans;
+
+
+//if when secret number is not 5
+if (secretNumber != 5 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=5 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+//if number is 5
+if (secretNumber == 5 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 5 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+
+	//when the number isnt 7///////////////////////////////////////////////////////////////
+cout<<"is the number 7: ";
+cin>> ans;
+
+
+
+//if when secret number is not 7
+if (secretNumber != 7 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=7 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+//if number is 7
+if (secretNumber == 7 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 7 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+
+	//when the number isnt 9///////////////////////////////////////////////////////////////
+	
+cout<<"is the number 9: ";
+cin>> ans;
+
+
+
+//if when secret number is not 9
+if (secretNumber != 9 && ans == 'y')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber !=9 && ans == 'n')
+{
+	cout<<"Option is correct then what is the number"<<endl;
+}
+
+
+//if number is 9
+if (secretNumber == 9 && ans == 'n')
+{
+	cout<<"incorrect"<<endl;
+}
+if (secretNumber == 9 && ans == 'y')
+{
+	cout<<"Option and Secret Number is correct"<<endl;
+}
+
+
+	return 0;
+}
